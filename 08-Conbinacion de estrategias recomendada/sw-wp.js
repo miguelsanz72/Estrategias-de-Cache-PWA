b8c1803eace23c0cf4355b58856d907c211e2163
Miguel Sanz <msanz1610@gmail.com>
2023-08-01 18:45:58 -0400

    funcionando sin errores

--- 08-Conbinacion de estrategias recomendada/sw-wp.js
+++ 08-Conbinacion de estrategias recomendada/sw-wp.js
@@ -23,16 +23,23 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request)
       .then(cachedResp => {
-        const networkFetch = fetch(e.request)
+        if (cachedResp) {
+          // If resource is in cache, fetch the network resource in the background to update cache
+          fetch(e.request)
+            .then(networkResp => {
+              caches.open(CACHE_STATIC_NAME).then(cache => cache.put(e.request, networkResp));
+            })
+            .catch(() => {});  // Ignore network errors, as cache will be used
+          return cachedResp;
+        }
+
+        // If resource is not in cache, fetch from network, cache it, and return it
+        return fetch(e.request)
           .then(networkResp => {
-            if (e.request.method !== 'POST') {
-              caches.open(CACHE_STATIC_NAME).then(cache => cache.put(e.request, networkResp.clone()));
-              cleanCache(CACHE_STATIC_NAME, 500);
-            }
+            const cloneResp = networkResp.clone();
+            caches.open(CACHE_STATIC_NAME).then(cache => cache.put(e.request, cloneResp));
             return networkResp;
           });
-
-        return cachedResp || networkFetch;
       })
       .catch(() => {
         if (e.request.headers.get("accept").includes("text/html")) {
