f620fbc482dc11cc3c401e48d77a38c8a2d455a9
Miguel Sanz <msanz1610@gmail.com>
2023-08-01 18:33:25 -0400

    only cache

--- 08-Conbinacion de estrategias recomendada/sw-wp.js
+++ 08-Conbinacion de estrategias recomendada/sw-wp.js
@@ -1,105 +1,60 @@
-/* eslint-disable consistent-return */
-/* eslint-disable max-len */
-/* eslint-disable no-undef */
-/* eslint-disable no-restricted-globals */
-
 const CACHE_STATIC_NAME = "static-v2";
 const CACHE_DINAMIC_NAME = "dinamic-v2";
 const CACHE_INMUTABLE_NAME = "inmutable-v2";
 
-// 2 Cache with network fallback, intenta leer enl cache, si no funciona busca en la red con limite de cache dinamico
-
 async function cleanCache(cacheName, maxItems) {
   const cache = await caches.open(cacheName);
-  cache.keys().then((keys) => {
-    if (keys.length > maxItems) {
-      cache.delete(keys[0]).then(cleanCache(cacheName, maxItems));
-    }
-  });
+  const keys = await cache.keys();
+  if (keys.length > maxItems) {
+    await cache.delete(keys[0]);
+    await cleanCache(cacheName, maxItems);
+  }
 }
 
-// prettier-ignore
 self.addEventListener('install', (e) => {
-    const cacheprom = caches
-        .open(CACHE_STATIC_NAME)
-        .then((cache) => cache.addAll([
-            './',
-        ]));
-
-        const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME )
-        .then((cache) => cache.addAll([
-            '/wp-includes/css/dashicons.min.css?ver=5.6',
-        ]));
-
-    e.waitUntil(Promise.all([cacheprom, cacheInmutable]));
+  const cacheStatic = caches.open(CACHE_STATIC_NAME).then(cache => cache.addAll(['./']));
+  const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME).then(cache => cache.addAll(['/wp-includes/css/dashicons.min.css?ver=5.6']));
+  e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 });
 
 self.addEventListener("fetch", (e) => {
   if (e.request.url.includes("wp-admin")) return;
 
-  const response = caches
-    .match(e.request)
-    .then((resp) => {
-      // En cada recarga va a la web y actualiza el STATIC CACHE
-      caches.open(CACHE_STATIC_NAME).then((cache) => {
-        fetch(e.request).then((newresp) => cache.put(e.request.clone(), newresp.clone()));
-        cleanCache(CACHE_STATIC_NAME, 500);
-      });
-
-      // No existe el archivo
-      // Va a la Web
-
+  const response = caches.match(e.request)
+    .then(resp => {
       if (resp) return resp;
 
-      return fetch(e.request).then((newResp) => {
+      return fetch(e.request).then(newResp => {
+        const cloneResp = newResp.clone();
         
-          if (e.request.method !== 'POST') {
-              caches.open(CACHE_DINAMIC_NAME).then((cache) => {
-                  cache.put(e.request.clone(), newResp.clone());
-                  cleanCache(CACHE_DINAMIC_NAME, 500);
-              });
-          }
-
-          cache.put(e.request.clone(), newResp.clone());
+        if (e.request.method !== 'POST') {
+          caches.open(CACHE_STATIC_NAME).then(cache => cache.put(e.request, cloneResp));
+          cleanCache(CACHE_STATIC_NAME, 500);
+        } else {
+          caches.open(CACHE_DINAMIC_NAME).then(cache => cache.put(e.request, cloneResp));
           cleanCache(CACHE_DINAMIC_NAME, 500);
+        }
+        
+        return newResp;
       });
-
-        return newResp.clone();
-    });
-
-    e.respondWith(response);
-    
-})
-.catch(() => {
+    })
+    .catch(() => {
       if (e.request.headers.get("accept").includes("text/html")) {
         return caches.match("/pages/offline.html");
       }
+    });
+
+  e.respondWith(response);
 });
 
-
-
-// cunado se activa el Sw
-
 self.addEventListener("activate", (e) => {
-  console.log("SW Activado!!");
-  // Borramos los static cache viejos o que no se estan usando
-  const respuesta = caches.keys().then((keys) => {
-    keys.forEach((key) => {
-      if (key !== CACHE_STATIC_NAME && key.includes("static")) {
-        return caches.delete(key);
-      }
-    });
-    keys.forEach((key) => {
-      if (key !== CACHE_DINAMIC_NAME && key.includes("dinamic")) {
-        return caches.delete(key);
-      }
-    });
-    keys.forEach((key) => {
-      if (key !== CACHE_INMUTABLE_NAME && key.includes("inmutable")) {
-        return caches.delete(key);
-      }
-    });
+  const clearOldCaches = caches.keys().then(keys => {
+    const keysToDelete = keys.filter(key => 
+      (key !== CACHE_STATIC_NAME && key.includes("static")) ||
+      (key !== CACHE_DINAMIC_NAME && key.includes("dinamic")) ||
+      (key !== CACHE_INMUTABLE_NAME && key.includes("inmutable"))
+    );
+    return Promise.all(keysToDelete.map(key => caches.delete(key)));
   });
-
-  e.waitUntil(respuesta);
+  e.waitUntil(clearOldCaches);
 });
